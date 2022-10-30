cea2b1a7d65e6a91387e31232a6a6b506bf7db49
raauf <vrairaaufabidi@gmail.com>
2022-10-30 14:09:02 +0100

    validation inputs with while loop

--- exercice2.cpp
+++ exercice2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string>
 int main()
 {
 	int age;
@@ -10,6 +10,11 @@ int main()
 		std::cin.clear();
 		std::cin.ignore(100, '\n');
 	}
-	std::cout << "age : "<<age<<std::endl;
+	std::cin.clear();
+	std::cin.ignore(100, '\n');
+	std::string name;
+	std::cout<<"enter your name : "<<std::endl;
+	std::getline(std::cin, name);
+	std::cout<<"name : "<<name<<std::endl << "age : "<<age<<std::endl;
 	return 0;
 }
