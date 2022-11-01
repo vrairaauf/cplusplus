3e1be321fd443bdcd85d5f04d67ffafc5a55268f
raauf <vrairaaufabidi@gmail.com>
2022-11-01 12:29:34 +0100

    std::max(int x, int y) it's a function return the max element between two elements

--- vector.cpp
+++ vector.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 
 int main(){
+	std::cout << "the max between 10 and 12 is : " << std::max(10, 12) << std::endl;
 	std::vector<int> v;
 	for(int cmp=0; cmp<10; cmp++)
 		v.push_back(cmp+1);
@@ -11,6 +12,7 @@ int main(){
 	v.push_back(2);
 	v.push_back(1);
 	sort(v.begin(), v.end());
+
 	for(int cmp=0; cmp<12; cmp++)
 		std::cout << v[cmp] << std::endl;
 	std::cout <<"size : " << v.size() << std::endl << "capacity : " << v.capacity() << std::endl;
