a731ace20d1c05f8b454beede38d62be0f9b7676
raauf <vrairaaufabidi@gmail.com>
2022-11-01 12:24:13 +0100

    sorting function in c++

---
+++ vector.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main(){
+	std::vector<int> v;
+	for(int cmp=0; cmp<10; cmp++)
+		v.push_back(cmp+1);
+	for(int cmp=0; cmp<10; cmp++)
+		std::cout << v[cmp] << std::endl;
+	v.push_back(2);
+	v.push_back(1);
+	std::sort(v.begin(), v.end());
+	for(int cmp=0; cmp<12; cmp++)
+		std::cout << v[cmp] << std::endl;
+	std::cout <<"size : " << v.size() << std::endl << "capacity : " << v.capacity() << std::endl;
+	return 0;
+}
