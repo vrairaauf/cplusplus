589e83adbbd1a77eef01052b2ba1571de2753587
raauf <vrairaaufabidi@gmail.com>
2022-11-01 12:33:43 +0100

    error management in c++ use try{}catch(...){} to capture any error in the program

--- vector.cpp
+++ vector.cpp
@@ -3,18 +3,23 @@
 #include <vector>
 
 int main(){
-	std::cout << "the max between 10 and 12 is : " << std::max(10, 12) << std::endl;
-	std::vector<int> v;
-	for(int cmp=0; cmp<10; cmp++)
-		v.push_back(cmp+1);
-	for(int cmp=0; cmp<10; cmp++)
-		std::cout << v[cmp] << std::endl;
-	v.push_back(2);
-	v.push_back(1);
-	sort(v.begin(), v.end());
+	try{
+		std::cout << "the max between 10 and 12 is : " << std::max(10, 12) << std::endl;
+		std::vector<int> v;
+		for(int cmp=0; cmp<10; cmp++)
+			v.push_back(cmp+1);
+		for(int cmp=0; cmp<10; cmp++)
+			std::cout << v[cmp] << std::endl;
+		v.push_back(2);
+		v.push_back(1);
+		sort(v.begin(), v.end());
 
-	for(int cmp=0; cmp<12; cmp++)
-		std::cout << v[cmp] << std::endl;
-	std::cout <<"size : " << v.size() << std::endl << "capacity : " << v.capacity() << std::endl;
+		for(int cmp=0; cmp<12; cmp++)
+			std::cout << v[cmp] << std::endl;
+		std::cout <<"size : " << v.size() << std::endl << "capacity : " << v.capacity() << std::endl;
+	}catch(...){
+		std::cout << "error in program"<<std::endl;
+		system("pause");
+	}
 	return 0;
 }
