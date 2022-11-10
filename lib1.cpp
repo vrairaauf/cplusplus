fb06288c96fd1965de3cbdf58deed0006b5091e4
raauf <vrairaaufabidi@gmail.com>
2022-11-10 09:20:31 +0100

    shared librairie and static librairie concept in c++

--- lib1.cpp
+++ lib1.cpp
@@ -1,6 +1,10 @@
-
+/*
+	g++ lib1.cpp -o sharedlib.dll -fpic -shared 
+	g++ -c mainshared.cpp -o mainshared.o
+	g++  mainshared.o -o mainshareda -L . -lsharedkib
+*/
 int add(int x, int y){
-	return x+y;
+	return x+y+10;
 }
 /*------------------*/
 int min(int x, int y){
---
+++ mainshared.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "lib1.h"
+int main(){
+	std::cout << add(5,4) << std::endl;
+	return 0;
+}
